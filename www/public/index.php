25a1be9a8072aa471cb8136d4e30974418c155e2
Robert-Dan <danrobert445@gmail.com>
2022-11-13 18:56:30 +0000

    app/core

--- www/public/index.php
+++ www/public/index.php
@@ -3,10 +3,10 @@
 require __DIR__.'/../../config/bootstrap.php';
 print '<pre>';
 
-#use App\Core;
+use App\Core;
 use App\Core\Http\Router;
 
-#$oCore = new Core();
+$oCore = new Core();
 $oHttp = new Router();
 
 var_dump($oHttp->invoking_callouts());
