e78e3eb8601b4f7ecaacce0414b8286c2516792a
Robert-Dan <danrobert445@gmail.com>
2022-11-26 18:16:47 +0000

    default value

--- sh/_git.sh
+++ sh/_git.sh
@@ -10,8 +10,9 @@ read -p "Enter your commit message: " message
 git commit -m "${message:=default value}"
 
 #git remote add origin https://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@github.com/johnidevo/action.git
+#git remote add origin https://robertddan:ATBBLeBCEAjMgjFkDehDxk2Bm8qe48C28E13@bitbucket.org/robo-workspace/roboziel.git
 
-git remote add origin https://robertddan:ATBBLeBCEAjMgjFkDehDxk2Bm8qe48C28E13@bitbucket.org/robo-workspace/roboziel.git
+git remote add origin https://robertddan:ATBBLeBCEAjMgjFkDehDxk2Bm8qe48C28E13@bitbucket.org/robo-workspace/zieltrader-scrum.git
 
 git push origin master
 #git pull origin master
