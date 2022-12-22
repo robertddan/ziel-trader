789b8b2981546933a6aef9ae35c48aa7a9c94267
Robert-Dan <danrobert445@gmail.com>
2022-12-22 17:48:57 +0000

    git

--- sh/_git.sh
+++ sh/_git.sh
@@ -9,11 +9,8 @@ git status
 read -p "Enter your commit message: " message
 git commit -m "${message:=default value}"
 
-#git remote add origin https://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@github.com/johnidevo/action.git
-
-git remote add github https://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@github.com/johnidevo/roboziel.git
-git remote add bucket https://robertddan:ATBBLeBCEAjMgjFkDehDxk2Bm8qe48C28E13@bitbucket.org/robo-workspace/roboziel-scrum.git
+#git remote add github https://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@github.com/johnidevo/roboziel.git
+#git remote add bucket https://robertddan:ATBBLeBCEAjMgjFkDehDxk2Bm8qe48C28E13@bitbucket.org/robo-workspace/roboziel-scrum.git
 
 git push github master
 git push bucket master
-#git pull origin master
