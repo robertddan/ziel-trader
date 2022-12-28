6ca44f81b56146c737ee11422373b5fa3a250f1b
Robert-Dan <danrobert445@gmail.com>
2022-12-28 18:41:47 +0000

    default value

--- core/db/dataziel.php
+++ core/db/dataziel.php
@@ -10,6 +10,11 @@ class Dataziel {
 	
 	public $sPath;
 	public $sTable;
+	public $iBytes;
+	
+	private $sRows;
+	private $sColumn;
+	
 	
 	function __construct($sPath, $sTable) {
 		$this->sPath = $sPath;
@@ -17,9 +22,27 @@ class Dataziel {
 		var_dump('-Sockets');
 	}
 	
-	function invoking_callouts() {
-		var_dump('--invoking_callouts');
-		var_dump($this->aQuery);
+	function invoking_callouts($sPrices) {
+		
+		$aPrices = json_decode();
+		
+		# Path
+		if (is_null($this->sRows)) $this->sRows = date('His');
+		if (is_null($this->sColumn)) $this->sColumn = date('Ymd');
+		
+		# Open files
+		$sFile = $this->sPath .'/'. $this->sTable .'/'. $this->sColumn .'/'. $this->sRows;
+		$this->rStreamHandler = fopen($sFile, 'w');
+		$bWrite = fwrite($this->rStreamHandler, $sPrices);
+		fflush($this->rStreamHandler);
+		if (!$bWrite) return false;
+		
+		# Rename
+		$aFstat = fstat($this->rStreamHandler);
+		if ($aFstat['size'] < $this->iBytes) $this->sRows = null;
+		if ($this->sColumn < date('Ymd')) $this->sColumn = null;
+		#fclose($this->rStreamHandler);
+		
 		return true;
 	}
 }
