7a38f92367c3c25425fb7a9693be66d668c83676
Robert-Dan <robertddanedu@gmail.com>
2025-02-20 17:12:24 -0500

    default value

--- assets/_apt.sh
+++ assets/_apt.sh
@@ -1,2 +1,8 @@
-which php
+#which php
+
+sudo apt install php-dev
+sudo apt-get install php-pear
+sudo pecl install trader
+
 php -i|grep 'php.ini'
+sudo sh -c 'echo "extension=trader.so" > /etc/php/8.2/cli/php.ini"
